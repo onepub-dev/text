4586c29ee5f7ab3c36cd32c5fa84a797a3257173
unknown <D:\MAIL>
2014-09-18 02:27:07 +0600

    0.0.3

--- example/example.dart
+++ example/example.dart
@@ -1,11 +1,3 @@
-text
-====
-
-The package 'text' allows to get detailed information about the text such as the number of lines, lines at positions, characters at locations, locations at positions, positions at locations, and vice versa.
-
-It is much faster than you could possibly imagine, thanks to the sparse lists.
-
-```dart
 import "package:text/text.dart";
 
 void main() {
@@ -46,5 +38,3 @@ String string = '''
 4. Line 4
 5. Line 5 ￠
 ''';
-
-```
