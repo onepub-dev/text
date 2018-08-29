2fe9b6bc9be799888d5c12fc805dfa94af0bf520
mezoni <andrew.mezoni@gmail.com>
2018-08-29 15:21:40 +0500

    0.1.0

--- test/test.dart
+++ test/test.dart
@@ -1,46 +1,69 @@
 import "package:text/text.dart";
-import "package:unittest/unittest.dart";
+import "package:test/test.dart";
 
 void main() {
-  var text = new Text(string);
-  expect(text.lineCount, 4, reason: "Text.lineCount");
-  //
-  var string2 = string.replaceAll("\n", "\r\n");
-  var text2 = new Text(string2);
-  expect(text2.lineCount, 4, reason: "Text.lineCount");
-  //
-  var text3 = new Text("");
-  expect(text3.lineCount, 1, reason: "Text.lineCount");
-  //
-  var text4 = new Text("\r");
-  expect(text4.lineCount, 1, reason: "Text.lineCount");
-  //
-  //
-  var text5 = new Text("\r ");
-  expect(text5.lineCount, 2, reason: "Text.lineCount");
-  //
-  var length = text2.length;
-  for (var position = 0; position < length; position++) {
-    var line = text2.lineAt(position);
-    expect(position >= line.start, true, reason: "Line.start");
-    expect(position <= line.end, true, reason: "Line.end");
-  }
-  //
-  length = text2.length;
-  for (var position = 0; position < length; position++) {
-    var line = text2.lineAt(position);
-    var location = text2.locationAt(position);
-    expect(location.line, line.number, reason: "Line.locationAt");
-    expect(location.column, position - line.start + 1, reason: "Line.locationAt");
-  }
-
-  //
-  length = text2.length;
-  for (var position = 0; position < length; position++) {
-    var location = text2.locationAt(position);
-    var position2 = text2.position(location);
-    expect(position2, position, reason: "Line.position");
-  }
+  test("Text", () {
+    {
+      var text = new Text(string);
+      expect(text.lineCount, 4, reason: "Text.lineCount");
+    }
+    //
+    {
+      var str = string.replaceAll("\n", "\r\n");
+      var text = new Text(str);
+      expect(text.lineCount, 4, reason: "Text.lineCount");
+    }
+    //
+    {
+      var text = new Text("");
+      expect(text.lineCount, 1, reason: "Text.lineCount");
+    }
+    //
+    {
+      var text = new Text("\r");
+      expect(text.lineCount, 1, reason: "Text.lineCount");
+    }
+    //
+    {
+      var text = new Text("\r ");
+      expect(text.lineCount, 2, reason: "Text.lineCount");
+    }
+    //
+    {
+      var str = string.replaceAll("\n", "\r\n");
+      var text = new Text(str);
+      var length = text.length;
+      for (var position = 0; position < length; position++) {
+        var line = text.lineAt(position);
+        expect(position >= line.start, true, reason: "Line.start");
+        expect(position <= line.end, true, reason: "Line.end");
+      }
+    }
+    //
+    {
+      var str = string.replaceAll("\n", "\r\n");
+      var text = new Text(str);
+      var length = text.length;
+      for (var position = 0; position < length; position++) {
+        var line = text.lineAt(position);
+        var location = text.locationAt(position);
+        expect(location.line, line.number, reason: "Line.locationAt");
+        expect(location.column, position - line.start + 1,
+            reason: "Line.locationAt");
+      }
+    }
+    //
+    {
+      var str = string.replaceAll("\n", "\r\n");
+      var text = new Text(str);
+      var length = text.length;
+      for (var position = 0; position < length; position++) {
+        var location = text.locationAt(position);
+        var position2 = text.position(location);
+        expect(position2, position, reason: "Line.position");
+      }
+    }
+  });
 }
 
 String string = '''
